2ee609f7ad8f775f53138e1803e6b016bb50c83c
Alexandre Duros <alexandre.duros@gmail.com>
2015-02-16 19:51:02 +0100

    added some tests for segment base and segment template

--- src/parser/DASHDemuxer_test.go
+++ src/parser/DASHDemuxer_test.go
@@ -2,6 +2,9 @@ package parser
 
 import "testing"
 
+const segmentBaseManifest string = "dash-vod-aka-test.canal-bis.com/test/1fps/index.mpd"
+const segmentTemplateManifest string = "www.digitalprimates.net/dash/streams/mp4-live-template/mp4-live-mpd-AV-BS.mpd"
+
 var demuxer DASHDemuxer
 
 func TestOpen(t *testing.T) {
@@ -19,5 +22,30 @@ func TestOpen(t *testing.T) {
     if c.want != c.got {
       t.Errorf("bad manfest url. want %q, got %q", c.want, c.got)
     }
+  }
+}
+
+func TestSegemenBaseGetTracks(t *testing.T) {
+  demuxer := new(DASHDemuxer)
+  demuxer.Open(segmentBaseManifest)
+  var tracks []*Track
+
+  err := demuxer.GetTracks(&tracks)
+
+  if err != nil {
+    t.Errorf("got error in GetTracks %q", err)
+  }
+
+}
+
+func TestSegemenTemplateGetTracks(t *testing.T) {
+  demuxer := new(DASHDemuxer)
+  demuxer.Open(segmentTemplateManifest)
+  var tracks []*Track
+
+  err := demuxer.GetTracks(&tracks)
+
+  if err != nil {
+    t.Errorf("got error in GetTracks %q", err)
   }
 }
